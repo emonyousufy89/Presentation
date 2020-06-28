0e40faa70bcc23d5c8011a12980c828f4791327f
emon yousufy <emonyousufy89@gmail.com>
2020-06-27 23:38:07 -0400

    this is a test

---
+++ src/test/java/talentech/presentation/sunday.java
@@ -0,0 +1,11 @@
+package talentech.presentation;
+
+public class sunday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Ayah, anwar, kazi, Emon");
+	}
+
+}
